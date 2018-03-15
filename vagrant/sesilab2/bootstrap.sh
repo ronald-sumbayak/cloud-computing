ff90535703b8ddd16f32ea5b3cd59a2f962f4f81
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-03-15 08:18:43 +0700

    Add nginx config to sesilab2

--- vagrant/sesilab2/bootstrap.sh
+++ vagrant/sesilab2/bootstrap.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get -y install nginx php5 php5-fpm php5-cgi
+sudo apt-get update
+sudo apt-get -y install nginx php5 php5-fpm php5-cgi
+
+sudo ln -s /vagrant/nginx.conf /etc/nginx/sites-enabled
+sudo nginx -t
+sudo service php7.2-fpm start
+sudo service nginx restart
