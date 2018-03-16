0d149dc62e2c531e0387a2e6decf2d6feec9664d
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-03-16 18:31:39 +0700

    Finish sesilab2

--- vagrant/sesilab2/bootstrap.sh
+++ vagrant/sesilab2/bootstrap.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+# install php
 sudo apt-get update
-sudo apt-get -y install nginx php5 php5-fpm php5-cgi
+sudo apt-get -y install python-software-properties software-properties-common
+sudo apt-add-repository -y ppa:ondrej/php
+sudo apt-get update
+sudo apt-get -y install php7.2
+sudo apt-get -y install php7.2-fpm php7.2-cgi
 
+# install nginx
+sudo apt-get -y --purge remove apache2
+sudo apt-get -y install nginx
+
+sudo rm -f /etc/nginx/sites-enabled/*
 sudo ln -s /vagrant/nginx.conf /etc/nginx/sites-enabled
 sudo nginx -t
 sudo service php7.2-fpm start
