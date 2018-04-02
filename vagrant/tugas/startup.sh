1ea609acfc5b03edd960554d5f515fc2b8d4de7c
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-04-02 20:49:40 +0700

    Remove cloning git pelatihan-laravel

--- vagrant/tugas/startup.sh
+++ vagrant/tugas/startup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-git clone git@github.com/fathoniadi/pelatihan-laravel.git
 sudo chown -R www-data:www-data pelatihan-laravel/storage pelatihan-laravel/bootstrap/cache
 sudo chmod -R 775 pelatihan-laravel/storage pelatihan-laravel/bootstrap/cache
 vagrant up
