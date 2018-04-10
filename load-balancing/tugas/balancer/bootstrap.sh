c38c7c2e8eabc3c34bd7a5e54e2c6aa83781f6cf
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-04-11 04:59:05 +0700

    Remove PHP installation from Provision

--- load-balancing/tugas/balancer/bootstrap.sh
+++ load-balancing/tugas/balancer/bootstrap.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
-# install php
-sudo apt-get update
-sudo apt-get -y install python-software-properties software-properties-common
-sudo apt-add-repository -y ppa:ondrej/php
-sudo apt-get update
-sudo apt-get -y install php7.2
-sudo apt-get -y install php7.2-fpm php7.2-cgi
-
 # install nginx
-sudo apt-get -y --purge remove apache2
+sudo apt-get -y update
 sudo apt-get -y install nginx
 
 sudo rm -f /etc/nginx/sites-enabled/*
 sudo ln -s /vagrant/balancer.conf /etc/nginx/sites-enabled
 sudo nginx -t
-sudo service php7.2-fpm start
 sudo service nginx restart
--- load-balancing/tugas/workers/bootstrap.sh
+++ load-balancing/tugas/workers/bootstrap.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/env bash
-
-# install php
-sudo apt-get update
-sudo apt-get -y install python-software-properties software-properties-common
-sudo apt-add-repository -y ppa:ondrej/php
-sudo apt-get update
-sudo apt-get -y install php7.2
-sudo apt-get -y install php7.2-fpm php7.2-cgi
-sudo service php7.2-fpm start
+# install apache2
+sudo apt-get -y update
+sudo apt-get -y install apache2
