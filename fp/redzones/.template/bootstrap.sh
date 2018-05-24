1f35b78bce0cc337976fa3862959160e7ed95748
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-05-24 18:42:11 +0700

    Add docker_images cache

--- fp/redzones/.template/bootstrap.sh
+++ fp/redzones/.template/bootstrap.sh
@@ -8,6 +8,11 @@ locale-gen en_US.UTF-8
 apt-get update
 apt-get -y install docker docker-compose
 
+imgs=(wordpress-4.7.1.tar.gz mysql-5.7.tar.gz phpmyadmin.tar.gz)
+for i in imgs; do
+    docker load -i docker_images/$i
+done
+
 git clone https://github.com/ndemoor/docker-wordpress-workflow-demo.git wpdc
 cd wpdc
 docker-compose up -d
